0d8d1c0c329e2ff9869cb19cead398da3c0abb8c
1BarCode <69029687+1BarCode@users.noreply.github.com>
2021-07-19 03:25:35 -0700

    update to login and sign up form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import { Container, Grid, Paper, Typography } from "@material-ui/core";
+import { Container, Grid, Paper, Typography, Button } from "@material-ui/core";
 import Input from "./Input";
 
 const initialFormData = {
@@ -12,18 +12,28 @@ const initialFormData = {
 };
 
 const Auth = () => {
-  const [wantSignUp, setWantSignUp] = useState(false);
+  const [wantSignUp, setWantSignUp] = useState(true);
   const [formData, setformData] = useState(initialFormData);
 
   const handleChange = (e) => {
-    setformData({ ...initialFormData, [e.target.name]: e.target.value });
+    setformData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSwitchMode = () => {
+    setWantSignUp((prevState) => !prevState);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
   };
 
   return (
     <Container maxWidth="xs">
       <Paper elevation={3}>
         <Typography>{wantSignUp ? "Sign Up" : "Sign In"}</Typography>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             {wantSignUp && (
               <>
@@ -52,7 +62,24 @@ const Auth = () => {
               type="password"
               handleChange={handleChange}
             />
+            {wantSignUp && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                type="password"
+                handleChange={handleChange}
+              />
+            )}
           </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            {wantSignUp ? "Sign Up" : "Sign In"}
+          </Button>
+          {/* Google Login Button */}
+          <Button onClick={handleSwitchMode}>
+            {wantSignUp
+              ? "Already have an account? Sign In"
+              : "Don't have an account? Sign Up"}
+          </Button>
         </form>
       </Paper>
     </Container>
--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Grid, TextField } from "@material-ui/core";
 
-const Input = ({ name, label, type, half }) => {
+const Input = ({ name, label, type, half, handleChange }) => {
   return (
     <Grid item xs={12} sm={half ? 6 : 12}>
-      <TextField name={name} label={label} type={type} />
+      <TextField
+        name={name}
+        label={label}
+        variant="outlined"
+        required
+        fullWidth
+        type={type}
+        onChange={handleChange}
+      />
     </Grid>
   );
 };
